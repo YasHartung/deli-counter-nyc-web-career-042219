31309855fb17bbf4f2ce65980351f72b784f1604
Yasmine Hartung <y.hartung@gmail.com>
2019-03-06 07:40:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,11 @@ def line (line)
     puts "The line is currently empty."
   else
     i=0
-    string = "The line is currently:"
+    puts "The line is currently:"
     while length > 0
-      string += " #{i+1}. #{line[1]}"
+      puts " #{i+1}. #{line[1]}"
       length -= 1
     end
-    return string
   end
 
 end
