ccd2fc0fa514f47e042098dc2178a9e0c837bee6
Yasmine Hartung <y.hartung@gmail.com>
2019-03-06 07:36:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line (line)
+  length = line.length
+
+  if length == 0
+    puts "The line is currently empty."
+  else
+    i=0
+    string = "The line is currently:"
+    while length > 0
+      string += " #{i+1}. #{line[1]}"
+      length -= 1
+    end
+  end
